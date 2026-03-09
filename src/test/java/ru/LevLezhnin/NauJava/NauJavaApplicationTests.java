415295d0f5306d683af50309570c5905c90cbc67
levLezhnin <lezhnin.lev78@gmail.com>
2026-03-09 21:06:51 +0500

    Initial commit

---
+++ src/test/java/ru/LevLezhnin/NauJava/NauJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.LevLezhnin.NauJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
